ffc8a84b67c189cea41697926ab96b6d767a0bbc
Mikesh Khanal <44762372+mkhanal1@users.noreply.github.com>
2019-02-26 11:05:17 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,18 @@
+import json
+import os
+
+username = "REPLACE_ME_QUALYS_USERNAME"
+password = "REPLACE_ME_QUALYS_PASSWORD"
+BASEURL = "REPLACE_ME_BASE_URL"
+accountID = 457721770691
+kurl = 'curl -k -s -u {}:{} -H "X-Requested-With:Curl" -H "Accept: application/json" -X "GET"  "{}/cloudview-api/rest/1.5/aws/evaluations/{}"'.format(username, password,BASEURL,accountID)
+eval = os.popen(kurl).read()
+count = len(json.loads(eval)['content'])
+
+for i in  range (1,count):
+        qurl = 'curl -k -s -u {}:{} -H "X-Requested-With:Curl" -H "Accept: application/json" -X "GET"  "{}/cloudview-api/rest/1.5/aws/evaluations/{}/resources/{}"'.format(username, password,BASEURL,accountID,i)
+        result = os.popen(qurl).read()
+        abc = json.loads(result)
+        for i in range(len(abc['content'])):
+                test = abc['content'][i]
+                print ((json.dumps(test)))
