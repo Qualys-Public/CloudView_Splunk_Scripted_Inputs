77593d18ef67fe0986c117a3152ea74d3f6b18a2
Mikesh Khanal <44762372+mkhanal1@users.noreply.github.com>
2019-03-05 12:13:37 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import os
 username = "REPLACE_ME_QUALYS_USERNAME"
 password = "REPLACE_ME_QUALYS_PASSWORD"
 BASEURL = "REPLACE_ME_BASE_URL"
-accountID = 457721770691
+accountID = REPLACE_ME_ACCOUNT_ID
 kurl = 'curl -k -s -u {}:{} -H "X-Requested-With:Curl" -H "Accept: application/json" -X "GET"  "{}/cloudview-api/rest/1.5/aws/evaluations/{}"'.format(username, password,BASEURL,accountID)
 eval = os.popen(kurl).read()
 count = len(json.loads(eval)['content'])
