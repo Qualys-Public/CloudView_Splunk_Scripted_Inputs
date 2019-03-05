845d5c4047f5cb47e4abb371e840ca695d01bce4
Mikesh Khanal <44762372+mkhanal1@users.noreply.github.com>
2019-03-05 14:26:46 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,14 +7,16 @@ BASEURL = "REPLACE_ME_BASE_URL"
 accountID = REPLACE_ME_ACCOUNT_ID
 kurl = 'curl -k -s -u {}:{} -H "X-Requested-With:Curl" -H "Accept: application/json" -X "GET"  "{}/cloudview-api/rest/1.5/aws/evaluations/{}"'.format(username, password,BASEURL,accountID)
 eval = os.popen(kurl).read()
-content = json.loads(eval)['content']
+evalcontent = json.loads(eval)['content']
 
-for i in  range (1,len(content)):
-        qurl = 'curl -k -s -u {}:{} -H "X-Requested-With:Curl" -H "Accept: application/json" -X "GET"  "{}/cloudview-api/rest/1.5/aws/evaluations/{}/resources/{}"'.format(username, password,BASEURL,accountID,i)
+for i in  range (len(evalcontent)):
+        cid = int(evalcontent[i]["controlId"])
+        qurl = 'curl -k -s -u {}:{} -H "X-Requested-With:Curl" -H "Accept: application/json" -X "GET"  "{}/cloudview-api/rest/1.5/aws/evaluations/{}/resources/{}"'.format(username, password,BASEURL,accountID,cid)
         result = os.popen(qurl).read()
-        abc = json.loads(result)
-        for i in range(len(abc['content'])):
-                test = abc['content'][i]
-                test["controlName"] = content[i]["controlName"]
-                test["controlId"] = content[i]["controlId"]
-                print ((json.dumps(test)))
+        resourceevaluation = json.loads(result)
+        count = len(resourceevaluation['content'])
+        for j in range(count):
+                resourcecontent = resourceevaluation['content'][j]
+                resourcecontent["controlName"] = evalcontent[i]["controlName"]
+                resourcecontent["controlId"] = evalcontent[i]["controlId"]
+                print ((json.dumps(resourcecontent)))
